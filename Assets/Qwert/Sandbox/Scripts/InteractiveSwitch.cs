391183e6784c1e638b73edcfb40e7a887231236d
Qwert <qwert.izayoi@gmail.com>
2024-12-28 22:59:54 +0900

    Sandboxシーンを追加

---
+++ Assets/Qwert/Sandbox/Scripts/InteractiveSwitch.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using UdonSharp;
+using UnityEngine;
+
+namespace Qwert.Sandbox
+{
+    public class InteractiveSwitch : UdonSharpBehaviour
+    {
+        [SerializeField] private string callback;
+        [SerializeField] private StaticObjectSync.StaticObjectSync[] staticObjectSyncs;
+
+        private void Start()
+        {
+            InteractionText = callback;
+        }
+
+        public override void Interact()
+        {
+            SendCustomEvent(callback);
+        }
+
+        public void TeleportTo()
+        {
+            for (var i = 0; i < staticObjectSyncs.Length; i++)
+            {
+                staticObjectSyncs[i].TeleportTo(
+                    staticObjectSyncs[i].transform.position + new Vector3(-0.1f, 0, 0),
+                    staticObjectSyncs[i].transform.rotation
+                );
+            }
+        }
+
+        public void TeleportToGlobally()
+        {
+            for (var i = 0; i < staticObjectSyncs.Length; i++)
+            {
+                staticObjectSyncs[i].TeleportToGlobally(
+                    staticObjectSyncs[i].transform.position + new Vector3(-0.1f, 0, 0),
+                    staticObjectSyncs[i].transform.rotation
+                );
+            }
+        }
+
+        public void Respawn()
+        {
+            for (var i = 0; i < staticObjectSyncs.Length; i++)
+            {
+                staticObjectSyncs[i].Respawn();
+            }
+        }
+
+        public void RespawnGlobally()
+        {
+            for (var i = 0; i < staticObjectSyncs.Length; i++)
+            {
+                staticObjectSyncs[i].RespawnGlobally();
+            }
+        }
+    }
+}
