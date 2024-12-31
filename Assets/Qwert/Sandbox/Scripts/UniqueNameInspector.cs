36d19bb306172d7401277d6d8e703615f44006c5
Qwert <qwert.izayoi@gmail.com>
2024-12-31 14:08:06 +0900

    Sandboxシーンを更新

---
+++ Assets/Qwert/Sandbox/Scripts/UniqueNameInspector.cs
@@ -0,0 +1,16 @@
+﻿using TMPro;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+public class UniqueNameInspector : UdonSharpBehaviour
+{
+    [SerializeField] private TextMeshProUGUI textMesh;
+
+    void Start()
+    {
+        textMesh.text = Networking.GetUniqueName(gameObject);
+    }
+}
