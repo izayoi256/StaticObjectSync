9e615d033e4487afcfe7145d3a65f3fd8a1abbef
Qwert <qwert.izayoi@gmail.com>
2025-01-22 09:02:29 +0900

    実験を追加

---
+++ Assets/Qwert/Sandbox/Scripts/SerializationOrderExaminePrimary.cs
@@ -0,0 +1,58 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+namespace Qwert.Sandbox
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class SerializationOrderExaminePrimary : UdonSharpBehaviour
+    {
+        [SerializeField] private SerializationOrderExamineSecondary secondary;
+        [SerializeField] private float serializationIntervalInSeconds = 0.5f;
+        [SerializeField] private int serializationDelayInFrames = 1;
+
+        [UdonSynced] private int _x;
+        public int x => _x;
+
+        private void Start()
+        {
+            SendCustomEventDelayedFrames(nameof(RequestSerializationPrimary), 1);
+        }
+
+        public override void OnDeserialization()
+        {
+            if (Utilities.IsValid(secondary))
+            {
+                if (_x - secondary._y == 1)
+                {
+                    Debug.Log($"Primary: OK (x = {_x}, y = {secondary._y}");
+                }
+                else
+                {
+                    Debug.LogError($"Primary: Something went wrong! (x = {_x}, y = {secondary._y}");
+                }
+            }
+        }
+
+        public void RequestSerializationPrimary()
+        {
+            if (!Networking.IsOwner(gameObject))
+            {
+                return;
+            }
+
+            _x++;
+            RequestSerialization();
+            SendCustomEventDelayedSeconds(nameof(RequestSerializationPrimary), serializationIntervalInSeconds);
+
+            if (Utilities.IsValid(secondary))
+            {
+                // ↓同期オブジェクト数と回数が多いとSomething went wrongが発生することがある
+                secondary.SendCustomEventDelayedFrames(nameof(SerializationOrderExamineSecondary.RequestSerializationSecondary), serializationDelayInFrames);
+
+                // ↓Something went wrongが発生する確率が明らかに上がる
+                // secondary.RequestSerializationSecondary();
+            }
+        }
+    }
+}
---
+++ Assets/Qwert/Sandbox/Scripts/SerializationOrderExamineSecondary.cs
@@ -0,0 +1,41 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+namespace Qwert.Sandbox
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class SerializationOrderExamineSecondary : UdonSharpBehaviour
+    {
+        [SerializeField] private SerializationOrderExaminePrimary primary;
+
+        [UdonSynced] public int _y;
+        public int y => _y;
+
+        public override void OnDeserialization()
+        {
+            if (Utilities.IsValid(primary))
+            {
+                if (primary.x == _y)
+                {
+                    Debug.Log($"Secondary: OK (x = {primary.x}, y = {_y}");
+                }
+                else
+                {
+                    Debug.LogError($"Secondary: Something went wrong! (x = {primary.x}, y = {_y}");
+                }
+            }
+        }
+
+        public void RequestSerializationSecondary()
+        {
+            if (!Networking.IsOwner(gameObject))
+            {
+                return;
+            }
+
+            _y++;
+            RequestSerialization();
+        }
+    }
+}
